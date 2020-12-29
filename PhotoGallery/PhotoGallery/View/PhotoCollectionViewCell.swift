9622b7012645fa23805f21d9d6cfb0afc6070e20
Pedro Trujillo V <ptrujillo@crunchy.co>
2020-12-29 17:12:07 -0500

    Added PhotoCollectionViewCell

---
+++ PhotoGallery/PhotoGallery/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoGallery
+//
+//  Created by Pedro Trujillo on 12/29/20.
+//  Copyright © 2020 Pedro Trujillo V. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
