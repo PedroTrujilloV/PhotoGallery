661f434738c52bc922e1c62a47e9b69032b76497
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-01 20:10:07 -0500

    Added feching by favorites to store

---
+++ PhotoGallery/PhotoGallery/Store/PhotoStore.swift
@@ -0,0 +1,73 @@
+//
+//  PhotoStore.swift
+//  PhotoGallery
+//
+//  Created by Pedro Trujillo on 12/30/20.
+//  Copyright © 2020 Pedro Trujillo V. All rights reserved.
+//
+
+import Foundation
+import Photos
+import Combine
+
+class PhotoStore: ObservableObject{
+    
+    var didChange = PassthroughSubject<PhotoStore,Never>()
+    
+    @Published var currentGallery:Array<PhotoViewModel> = []{
+        didSet{ didChange.send(self) }
+    }
+    
+    private var byFavoriteGallery:Array<PhotoViewModel> = [] 
+    
+    private var byDateGallery:Array<PhotoViewModel> = []
+    
+    @Published var allPhotos:PHFetchResult<PHAsset> {
+        didSet{ didChange.send(self) }
+    }
+    
+    var smarthAlbums: PHFetchResult<PHAssetCollection> 
+    
+    var userCollections: PHFetchResult<PHAssetCollection>
+    
+    init(){
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [ NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
+        smarthAlbums = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumFavorites, options: nil)
+        userCollections = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: nil)
+    }
+    
+    func fetchByFavorite(){
+        if let collection = smarthAlbums.firstObject {
+            let fetchResult = PHAsset.fetchAssets(in: collection, options: nil)
+            for i in 0..<fetchResult.count {
+                let asset = fetchResult.object(at: i)
+                print(asset)
+                let vm = PhotoViewModel(asset)
+                byFavoriteGallery.append(vm)
+            }
+            currentGallery = byFavoriteGallery
+        } else {
+            print("PhotoStore.fetchByFavorite: Problem fetching by favorite")
+        }
+    }
+    
+    func fetchByDate(){
+        print(userCollections)
+        if let collection = userCollections.firstObject {
+            print(collection)
+            let fetchResult = PHAsset.fetchAssets(in: collection, options: nil)
+            for i in 0..<fetchResult.count {
+                let asset = fetchResult.object(at: i)
+                print(asset)
+//                let vm = PhotoViewModel(asset)
+//                byFavoriteGallery.append(vm)
+            }
+        }
+    }
+ 
+}
+
+
---
+++ PhotoGallery/PhotoGallery/ViewModel/PhotoViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoViewModel.swift
+//  PhotoGallery
+//
+//  Created by Pedro Trujillo on 12/30/20.
+//  Copyright © 2020 Pedro Trujillo V. All rights reserved.
+//
+
+import Foundation
