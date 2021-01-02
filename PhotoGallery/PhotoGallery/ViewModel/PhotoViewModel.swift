d473da6592c9eb8233304306efb793e8cec8d6a7
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-01 20:12:38 -0500

    Wraped image request in view model for customization

--- PhotoGallery/PhotoGallery/ViewModel/PhotoViewModel.swift
+++ PhotoGallery/PhotoGallery/ViewModel/PhotoViewModel.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import Photos
+import UIKit
+
+class PhotoViewModel {
+    
+    let model: PHAsset
+    private let imageManager = PHCachingImageManager()
+    let scale = UIScreen.main.scale
+    
+    var creationDate:Date? {
+        return model.creationDate
+    }
+
+    var location:CLLocation? {
+        return model.location
+    }
+    var favorite:Bool {
+        return model.isFavorite
+    }
+    
+    init(_ model: PHAsset) {
+        self.model = model
+    }
+    
+    func requestImage(cellSize: CGSize, completion:@escaping (UIImage) -> Void ) {
+        let thumbnailSize = CGSize(width: cellSize.width * scale, height: cellSize.height * scale)
+        imageManager.requestImage(for: model,
+                                  targetSize: thumbnailSize,
+                                  contentMode: .aspectFit,
+                                  options: nil,
+                                  resultHandler: { image, _ in
+                                    
+                                    if let image = image {
+                                        completion(image)
+                                    }
+        })
+    }
+}
+
